83933f73ba8b6b2c455215af0d4d89ea44878e90
Eric Trenkel <eric.trenkel@gmail.com>
2018-06-19 17:10:37 +0200

    Added Android support showInterstitial

--- README.md
+++ README.md
@@ -1,8 +1,25 @@
 # flutter_applovin
 
-A new Flutter applovin plugin.
+A new simple Flutter applovin demonstration plugin. Currently only supports interstitials. Pull 
+requests are welcome.
+
+## iOS
+
+Add your AppLovinSDK key in custom ios target properties under the key AppLovinSdkKey.
+
+## Android
+
+Add this in AndroidManifest.xml:
+
+    <meta-data android:name="applovin.sdk.key"
+        android:value="YOUR_SDK_KEY_ANDROID"
+        />
 
 ## Getting Started
+
+Load and show interstitial ad with:
+
+    FlutterApplovin.showInterstitial;
 
 For help getting started with Flutter, view our online
 [documentation](https://flutter.io/).
--- android/src/main/java/com/bostrot/flutterapplovin/FlutterApplovinPlugin.java
+++ android/src/main/java/com/bostrot/flutterapplovin/FlutterApplovinPlugin.java
@@ -1,5 +1,7 @@
 package com.bostrot.flutterapplovin;
 
+import android.content.Context;
+
 import com.applovin.adview.AppLovinInterstitialAd;
 import com.applovin.adview.AppLovinInterstitialAdDialog;
 import com.applovin.sdk.AppLovinAd;
@@ -21,38 +23,39 @@ public class FlutterApplovinPlugin implements MethodCallHandler {
    * Plugin registration.
    */
   static private AppLovinAd loadedAd;
+  static Context _context;
   public static void registerWith(final Registrar registrar) {
+    _context = registrar.context();
     final MethodChannel channel = new MethodChannel(registrar.messenger(), "flutter_applovin");
     channel.setMethodCallHandler(new FlutterApplovinPlugin());
     AppLovinSdk.initializeSdk(registrar.context());
-    // Load an Interstitial Ad
-    AppLovinSdk.getInstance( registrar.context() ).getAdService().loadNextAd( AppLovinAdSize.INTERSTITIAL,
-            new
-            AppLovinAdLoadListener()
-    {
-      @Override
-      public void adReceived(AppLovinAd ad)
-      {
-        loadedAd = ad;
-        AppLovinInterstitialAdDialog interstitialAd = AppLovinInterstitialAd.create( AppLovinSdk
-                .getInstance( registrar.context() ), registrar.context() );
-
-
-        interstitialAd.showAndRender( loadedAd );
-      }
-
-      @Override
-      public void failedToReceiveAd(int errorCode)
-      {
-        // Look at AppLovinErrorCodes.java for list of error codes.
-      }
-    } );
   }
 
   @Override
   public void onMethodCall(MethodCall call, Result result) {
-    if (call.method.equals("getPlatformVersion")) {
-      result.success("Android " + android.os.Build.VERSION.RELEASE);
+    if (call.method.equals("interstitial")) {
+      // Load an Interstitial Ad
+      AppLovinSdk.getInstance( _context ).getAdService().loadNextAd( AppLovinAdSize.INTERSTITIAL,
+              new
+                      AppLovinAdLoadListener()
+                      {
+                        @Override
+                        public void adReceived(AppLovinAd ad)
+                        {
+                          loadedAd = ad;
+                          AppLovinInterstitialAdDialog interstitialAd = AppLovinInterstitialAd.create( AppLovinSdk
+                                  .getInstance( _context ), _context );
+
+
+                          interstitialAd.showAndRender( loadedAd );
+                        }
+
+                        @Override
+                        public void failedToReceiveAd(int errorCode)
+                        {
+                          // Look at AppLovinErrorCodes.java for list of error codes.
+                        }
+                      } );
     } else {
       result.notImplemented();
     }
